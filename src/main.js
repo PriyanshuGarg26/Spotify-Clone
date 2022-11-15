e2b9756415c7195cdc5ff0d4f014b0f6a7b2908f
Priyanshu Garg <gargpriyanshu26@gmail.com>
2022-11-15 12:53:32 +0530

    upload spotify clone files

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import './style.css';
+const APP_URL = import.meta.env.VITE_APP_URL;
+
+document.addEventListener("DOMContentLoaded", () => {
+  if (localStorage.getItem("accessToken")) {
+    // window.location.href = `${APP_URL}/dashboard/dashboard.html`;
+    window.location.href = './dashboard/dashboard.html';
+  } else {
+    // window.location.href = `${APP_URL}/login/login.html`;
+    window.location.href = './login/login.html' ;
+  }
+})
+
